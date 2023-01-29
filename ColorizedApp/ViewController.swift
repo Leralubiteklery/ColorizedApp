0292252501b164514c23dcb8f10c0a14d42d817e
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-01-29 09:56:08 +0300

    Added change color functionality

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        setUpSliders()
     }
-
-
+    
+    private func setUpSliders() {
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    private func setViewColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        print(redSlider.value, blueSlider.value, greenSlider.value)
+    }
+    
+    @IBAction func redSliderMoved(_ sender: Any) {
+        redLabel.text = String(redSlider.value)
+        setViewColor()
+    }
+    
+    @IBAction func greenSliderMoved(_ sender: Any) {
+        greenLabel.text = String(greenSlider.value)
+        setViewColor()
+    }
+    
+    @IBAction func blueSliderMoved(_ sender: Any) {
+        blueLabel.text = String(blueSlider.value)
+        setViewColor()
+    }
 }
 
